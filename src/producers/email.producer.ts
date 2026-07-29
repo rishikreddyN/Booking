699d6f9fb3855cdb7acfd7b54b951d3d27c2ca05
Rishik Reddy <nallarishik58@gmail.com>
2026-07-29 23:07:28 +0530

    added notification service

---
+++ src/producers/email.producer.ts
@@ -0,0 +1,10 @@
+import {mailerQueue} from "../queues/mailer.queue";
+
+import {NotificationDto} from "../dto/notification.dto";
+
+export const MAILER_PAYLOAD = "payload:mail";
+
+export const addEmailToQueue = async (payload: NotificationDto) => {
+    await mailerQueue.add(MAILER_PAYLOAD, payload);
+    console.log(`Email added to queue: ${JSON.stringify(payload)}`);
+}
